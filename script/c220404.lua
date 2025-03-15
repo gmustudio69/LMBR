ff41a2fb437db762794de02b4b4bf0476226d874
Nebby <65876365+gmustudio69@users.noreply.github.com>
2025-03-15 15:06:59 +0700

    Update c220404.lua

--- script/c220404.lua
+++ script/c220404.lua
@@ -1,3 +1,4 @@
+
 local s,id,o=GetID()
 function s.initial_effect(c)
 	--xyz summon
@@ -31,28 +32,25 @@ function s.initial_effect(c)
 	e3:SetOperation(s.negop)
 	c:RegisterEffect(e3)
 	local e4=e3:Clone()
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetCountLimit(1,id+o*2)
-	e3:SetCondition(s.qcon)
-	e3:SetCode(EVENT_CHAINING)
-	c:RegisterEffect(e3)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCountLimit(1,id+o*2)
+	e4:SetCondition(s.qcon)
+	e4:SetCode(EVENT_CHAINING)
+	c:RegisterEffect(e4)
 	Duel.AddCustomActivityCounter(id,ACTIVITY_CHAIN,s.chainfilter)
 end
 function s.chainfilter(re,tp,cid)
 	local rc=re:GetHandler()
 	return not rc:IsCode(220406)
 end
-
 function s.ovfilter(c)
-	return c:IsFaceup()  and c:IsLevel(7) and c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_WARRIOR)
+	return c:IsFaceup() and c:IsLevel(7) and c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_WARRIOR)
 end
 function s.qcon(e,tp,eg,ep,ev,re,r,rp)
-	return rp==1-tp Duel.IsExistingMatchingCard(s.sprfilter,tp,LOCATION_ONFIELD,0,1,nil)
+	return rp==1-tp and Duel.IsExistingMatchingCard(s.sprfilter,tp,LOCATION_ONFIELD,0,1,nil)
 end
 function s.xyzop(e,tp,chk)
-	if chk==0 then return Duel.GetFlagEffect(tp,id)==0
-		and (Duel.GetCustomActivityCount(id,tp,ACTIVITY_CHAIN)>0
-			or Duel.GetCustomActivityCount(id,1-tp,ACTIVITY_CHAIN)>0) end
+	if chk==0 then return Duel.GetFlagEffect(tp,id)==0 and (Duel.GetCustomActivityCount(id,tp,ACTIVITY_CHAIN)>0 or Duel.GetCustomActivityCount(id,1-tp,ACTIVITY_CHAIN)>0) end
 	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,EFFECT_FLAG_OATH,1)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
